1f11a070b29fa6b390b6d3c64d77bae38792f14f
Ryan Leung <44559935+lionwok@users.noreply.github.com>
2018-11-17 11:46:24 -0800

    Revert "Update ManualSteering/joystick-servo/joystick-servo.ino"
    This reverts commit 312753f2c70c8cee01ff88eb699c6acea3ac51db.

--- ManualSteering/joystick-servo/joystick-servo.ino
+++ ManualSteering/joystick-servo/joystick-servo.ino
@@ -1,38 +1,37 @@
 #include <Servo.h>
-
+// Uncomment when using Duemilanove
 const int joyXPin = A0;
 const int joyYPin = A1;
 const int xservoPin = 9;
 const int yservoPin = 10;
-const int manualSwitchPin = 11; 
 
+// Uncomment when using MEGA
+/*
+const int joyXPin = 14;
+const int joyYPin = 15;
+*/
 int joystick_y_in;
 int joystick_x_in;
-int manualSwitch;
 
-Servo xservo;
+Servo myservo;
 Servo yservo;
+
 
 void setup() {
   Serial.begin(9600);
   delay(200);
-  xservo.attach(xservoPin);
+  myservo.attach(xservoPin);
   yservo.attach(yservoPin);
-  pinMode(manualSwitchPin, INPUT_PULLUP);
 }
 
 void loop() {
-  manualSwitch = digitalRead(manualSwitchPin);
-  if (manualSwitch == 1){
-    readInput();
-    xservo.write(joystick_x_in);
-    yservo.write(joystick_y_in);
-    Serial.print("X:");
-    Serial.print(joystick_x_in);
-    Serial.print(" Y:");
-    Serial.println(joystick_y_in);
-  }
-  else Serial.println("Manual steering is off.");
+  readInput();
+  myservo.write(joystick_x_in);
+  yservo.write(joystick_y_in);
+  Serial.print("X:");
+  Serial.print(joystick_x_in);
+  Serial.print(" Y:");
+  Serial.println(joystick_y_in);
   delay(30);
 }
 
@@ -40,9 +39,10 @@ void readInput() {
   joystick_x_in = analogRead(joyXPin);
   joystick_y_in = analogRead(joyYPin);
 
+  //joystick_x_in = betterMap(joystick_x_in, 101, 922, 60, 120);
+  //joystick_y_in = betterMap(joystick_y_in, 101, 923, 60, 120);
   joystick_x_in = betterMap(joystick_x_in, 101, 922, 0, 180);
   joystick_y_in = betterMap(joystick_y_in, 101, 923, 0, 180);
-  
   if (joystick_x_in < 97 && joystick_x_in > 83) {
     joystick_x_in = 90;
   }
@@ -51,6 +51,6 @@ void readInput() {
   }
 }
 
-int betterMap(int x, int in_min, int in_max, int out_min, int out_max) {
+long betterMap(long x, long in_min, long in_max, long out_min, long out_max) {
   return (x - in_min) * (out_max - out_min + 1) / (in_max - in_min + 1) + out_min;
 }
