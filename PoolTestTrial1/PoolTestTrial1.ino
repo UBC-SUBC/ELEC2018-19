ba837c548571fc6528fcd938d7105c9d00b843cf
Yuki Sekimori <yuki.sekimori.8@gmail.com>
2019-05-19 16:57:40 -0700

    Yuki Update PoolTestTrial1.ino
    Working SD card logging code for pool test.
    Needs edit on choosing the data to be written on the SD.

--- PoolTestTrial1/PoolTestTrial1.ino
+++ PoolTestTrial1/PoolTestTrial1.ino
@@ -1,12 +1,40 @@
-// Buoyancy Compensator
-
 /*
-// Simple buoyancy compensator code
-// 
+// Reading current values to the SD card 
+// Reference Code:
+// https://www.arduino.cc/en/Tutorial/ReadWrite
+// ELEC steering and Buoyancy Compensator
 */
 /* Things to add later
  * Serial read so that it can change the setup from the serial command.
  */
+
+/*****SD CARD WRITING SETUP*****/
+
+#include <SPI.h>
+#include <SD.h>
+
+File PoolTest;
+int loopCount;
+/*****SD CARD WRITING SETUP OVER*****/
+
+/*****ELEC STEERING START*****/
+
+#include <Servo.h>
+
+const int joyXPin = A0;
+const int joyYPin = A1;
+const int xservoPin = 9;
+const int yservoPin = 10;
+const int manualSwitchPin = 11; 
+
+int joystick_y_in;
+int joystick_x_in;
+int manualSwitch;
+
+Servo xservo;
+Servo yservo;
+
+/****ELEC STEERING END****/
 
 /*****BUOYANCY INTRO*****/
 
@@ -29,16 +57,15 @@
 // Ranging from (0 - 255)
 #define motorPWM 50
 
-
 /******Arudino pin definitions******/
 // Pins connected to Pololu Motor Driver
 #define dirPin 2 // motor direction output pin
 #define pwmPin 3 // pulse width modulation output pin
 #define slpPin 4 // sleep mode output pin. default LOW. 
                   // Must be set to HIGH to enable the driver
-#define fltPin 10 // fault indicator input pin. 
+//#define fltPin 10 // fault indicator input pin. 
                   // This is driven LOW when fault has occurred.
-#define csPin A1 // current sense output (analog input pin)
+//#define csPin A1 // current sense output (analog input pin)
 // Pins connected to the motor hall sensors (input pin)
 #define hs1Pin 7
 #define hs2Pin 8
@@ -51,75 +78,98 @@
 int mode;
 /*****BUOYANCY INTRO DONE*****/
 
-/*****ELEC STEERING START*****/
-
-#include <Servo.h>
-
-const int joyXPin = A0;
-const int joyYPin = A1;
-const int xservoPin = 9;
-const int yservoPin = 10;
-const int manualSwitchPin = 11; 
-
-int joystick_y_in;
-int joystick_x_in;
-int manualSwitch;
-
-Servo xservo;
-Servo yservo;
-
-/****ELEC STEERING END****/
-
-/*****SD CARD WRITING SETUP*****/
-
-#include <SPI.h>
-#include <SD.h>
-
-File PoolTest1;
-
-/*****SD CARD WRITING SETUP OVER*****/
 
 void setup()
 {
- //Buoyancy setup
+ /*****SD Card to Serial Port setup START****/ 
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(4)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+  Serial.println("initialization done.");
+  loopCount = 0; //set loop count to 0
+ /*****SD Card to Serial Port setup END****/ 
  
-  //Serial.begin(9600);
-  // setup digital pin modes
+  //buoyancy compensator setup
   pinMode(dirPin, OUTPUT);
   pinMode(slpPin, OUTPUT);
-  pinMode(fltPin, INPUT);
+  //pinMode(fltPin, INPUT);
   pinMode(hs1Pin, INPUT);
   pinMode(hs2Pin, INPUT);
   pinMode(ls1Pin, INPUT);
   pinMode(ls2Pin, INPUT);
-  pinMode(csPin, INPUT);
+  //pinMode(csPin, INPUT);
   pinMode(psPin, INPUT);
-  // if analog input pin 0 is unconnected, random analog
-  // noise will cause the call to randomSeed() to generate
-  // different seed numbers each time the sketch runs.
-  // randomSeed() will then shuffle the random function.
-  randomSeed(analogRead(0));
 
   //elec steering servo setup 
-  
-  //Serial.begin(9600);
   delay(200);
   xservo.attach(xservoPin);
   yservo.attach(yservoPin);
   pinMode(manualSwitchPin, INPUT_PULLUP);
-
-  //SD Card setup 
-  
-  // Open serial communications and wait for port to open:
-  //Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  
-}
+ 
 }
 
-/**********BUOYANCY MAIN FUNCTION**********/
+/*************MAIN LOOP***************/
 void loop() {
+/******CREATING FILES IN SD CARD STARTS HERE*******/
+  if (loopCount > 999){
+    // close the file after 1000 loops before making a new one
+    PoolTest.close();
+    Serial.println("file closed.");
+    loopCount = 0;
+  }
+  if (loopCount == 0){
+    // open the file. note that only one file can be open at a time,
+    // so you have to close this one before opening another.
+    char filename[] = "DATA000.CSV";
+    for (uint8_t i = 0; i < 500; i++) {
+        filename[4] = i/100 + '0';
+        filename[5] = i%100 + '0';
+        filename[6] = i%10 + '0';
+        if (! SD.exists(filename)) {
+          // only open a new file if it doesn't exist
+          PoolTest = SD.open(filename, FILE_WRITE);
+          break;  // leave the loop!
+        }
+    }
+    // if the file opened okay, write to it:
+    if (PoolTest) {
+    Serial.print("Writing to a new file");
+    PoolTest.println("apple, banana, cabbage"); //header line
+    } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening file");
+    }  
+  }
+  loopCount++;
+/******CREATING FILES IN SD CARD ENDS HERE*******/
+
+  /*****ELEC STEERING STARTS HERE*******/
+  manualSwitch = digitalRead(manualSwitchPin);
+  if (manualSwitch == 1){
+    Serial.println("Manual steering is on!");
+    readInput();
+    xservo.write(joystick_x_in);
+    yservo.write(joystick_y_in);
+    Serial.print("X:");
+    Serial.print(joystick_x_in);
+    Serial.print(" Y:");
+    Serial.println(joystick_y_in);
+  }
+  else Serial.println("Manual steering is off.");
+  delay(30);
+  /*****ELEC STEERING ENDS HERE*******/
+  
+ 
+ /**********BUOYANCY MAIN FUNCTION STARTS HERE**********/
  // readSensor();
   if (digitalRead(ls1Pin) == 0 && digitalRead(ls2Pin) == 0) {
     
@@ -134,10 +184,13 @@ void loop() {
     mode = 2;
   }
   motorDriver(mode);
+  /**********BUOYANCY MAIN FUNCTION ENDS HERE**********/
+  
 }
 
-/********FUNCTIONS********/
 
+         /********FUNCTIONS********/
+/********BUOYANCY COMPENSATOR FUNCTIONS******/
 /***************MOTOR DRIVER****************/
 void motorDriver(int motorMode){
   // Check if the motor enable is assingned properly
@@ -151,43 +204,42 @@ void motorDriver(int motorMode){
         digitalWrite(dirPin, motorMode); // assign the mode of the motor
         digitalWrite(slpPin, motorEnable); // assign the slp pin of the driver
         analogWrite(pwmPin, motorPWM); // assign the pwm value of the motor
-        //Serial.print("MODE:");
-        //Serial.print(motorMode); // motor mode
-        //Serial.print("\t");
-        //Serial.print("PWM:");
-        //Serial.print(motorPWM); // motor PWM
-        //Serial.print("\t");
+        Serial.print("MODE:");
+        Serial.print(motorMode); // motor mode
+        Serial.print("\t");
+        Serial.print("PWM:");
+        Serial.print(motorPWM); // motor PWM
+        Serial.print("\t");
       }
       
       else {
         digitalWrite(slpPin, LOW); // turn off the motor driver
-        //Serial.println("Error: motor pwm value not assingned in a correct range");
+        Serial.println("Error: motor pwm value not assingned in a correct range");
       }
     }
     else if(motorMode == BR){
       analogWrite(pwmPin, LOW); // assign LOW to pwmPin to break
-      //Serial.print("MODE:2\t"); // motor mode (BR = 2)
-      //Serial.print("PWM:0\t"); // motor mode (LOW = 0)
+      Serial.print("MODE:2\t"); // motor mode (BR = 2)
+      Serial.print("PWM:0\t"); // motor mode (LOW = 0)
     }
     
     else {
       digitalWrite(slpPin, LOW); // turn off the motor driver
-      //Serial.println("Error: motor mode not assigned correctly.");
+      Serial.println("Error: motor mode not assigned correctly.");
     }
   }
 
   else {
     digitalWrite(slpPin, LOW); // turn off the motor driver
-    //Serial.println("Error: motor driver not enabled correctly");
+    Serial.println("Error: motor driver not enabled correctly");
   }
 }
 
 /**********READING SENSOR INPUTS**************/
-/*
 void readSensor() {
   // Read the input values
   Serial.print("FLT:");
-  Serial.print(digitalRead(fltPin)); // FLT pin readings
+  //Serial.print(digitalRead(fltPin)); // FLT pin readings
   Serial.print("\t");
   Serial.print("HS1:");
   Serial.print(digitalRead(hs1Pin)); // hall sensor 1 readings
@@ -204,12 +256,11 @@ void readSensor() {
   Serial.print("PS:");
   Serial.print(analogRead(psPin)); // pressure sensor readings
   Serial.print("\t");
-  Serial.print("CS:");
-  Serial.print(currentSensor(analogRead(csPin))); // current sensor readings
-  Serial.print("[A]");
+  //Serial.print("CS:");
+  //Serial.print(currentSensor(analogRead(csPin))); // current sensor readings
+  //Serial.print("[A]");
   Serial.print("\n");
 }
-/
 
 /****CURRENT SENSOR CONVERSION*****/
 double currentSensor(int cs){
@@ -218,28 +269,7 @@ double currentSensor(int cs){
   return i;
 }
 
-
-//
-/*****Elec Steering joystick servo MAIN FUNCTION*****/
-//
-
-
-
-void loop2() {
-  manualSwitch = digitalRead(manualSwitchPin);
-  if (manualSwitch == 1){
-    //Serial.println("Manual steering is on!");
-    readInput();
-    xservo.write(joystick_x_in);
-    yservo.write(joystick_y_in);
-    //Serial.print("X:");
-    //Serial.print(joystick_x_in);
-    //Serial.print(" Y:");
-    //Serial.println(joystick_y_in);
-  }
-  else Serial.println("Manual steering is off.");
-  delay(30);
-}
+/*****ELEC STEERING FUNCTIONS*****/
 
 void readInput() {
   joystick_x_in = analogRead(joyXPin);
@@ -259,53 +289,4 @@ void readInput() {
 
 int betterMap(int x, int in_min, int in_max, int out_min, int out_max) {
   return (x - in_min) * (out_max - out_min + 1) / (in_max - in_min + 1) + out_min;
-}
-
-
-/*****SD card writing***/
-
-/*
-  SD card read/write
-
-  This example shows how to read and write data to and from an SD card file
-  The circuit:
-   SD card attached to SPI bus as follows:
- ** MOSI - pin 11
- ** MISO - pin 12
- ** CLK - pin 13
- ** CS - pin 4 (for MKRZero SD: SDCARD_SS_PIN)
-
-  created   Nov 2010
-  by David A. Mellis
-  modified 9 Apr 2012
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-*/
-
-  //Serial.print("Initializing SD card...");
-
-  if (!SD.begin(4)) {
-    //Serial.println("initialization failed!");
-    while (1);
-  }
-  //Serial.println("initialization done.");
-
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  PoolTest1 = SD.open("test.txt", FILE_WRITE);
-
-  // if the file opened okay, write to it:
-  if (PoolTest1) {
-    //Serial.print("Writing to test.txt...");
-    PoolTest1.println("testing 1, 2, 3.");
-    // close the file:
-    PoolTest1.close();
-    //Serial.println("done.");
-  } else {
-    // if the file didn't open, print an error:
-    //Serial.println("error opening test.txt");
-  }
-
 }
