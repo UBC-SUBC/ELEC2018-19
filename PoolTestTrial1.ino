62f34ba428a3c171b7e16bc76fa8d996e961d05a
katherinewest <kat.westerlund@gmail.com>
2019-05-19 14:05:54 -0700

    Update PoolTestTrial1.ino
    Updated things

--- PoolTestTrial1.ino
+++ PoolTestTrial1.ino
@@ -8,6 +8,7 @@
  * Serial read so that it can change the setup from the serial command.
  */
 
+/*****BUOYANCY INTRO*****/
 
 /************MOTOR SETUP***********/
 // motor on(HIGH)/off(LOW)
@@ -48,10 +49,41 @@
 #define psPin A2
 
 int mode;
+/*****BUOYANCY INTRO DONE*****/
+
+/*****ELEC STEERING START*****/
+
+#include <Servo.h>
+
+const int joyXPin = A0;
+const int joyYPin = A1;
+const int xservoPin = 9;
+const int yservoPin = 10;
+const int manualSwitchPin = 11; 
+
+int joystick_y_in;
+int joystick_x_in;
+int manualSwitch;
+
+Servo xservo;
+Servo yservo;
+
+/****ELEC STEERING END****/
+
+/*****SD CARD WRITING SETUP*****/
+
+#include <SPI.h>
+#include <SD.h>
+
+File PoolTest1;
+
+/*****SD CARD WRITING SETUP OVER*****/
 
 void setup()
 {
-  Serial.begin(9600);
+ //Buoyancy setup
+ 
+  //Serial.begin(9600);
   // setup digital pin modes
   pinMode(dirPin, OUTPUT);
   pinMode(slpPin, OUTPUT);
@@ -67,12 +99,28 @@ void setup()
   // different seed numbers each time the sketch runs.
   // randomSeed() will then shuffle the random function.
   randomSeed(analogRead(0));
+
+  //elec steering servo setup 
+  
+  //Serial.begin(9600);
+  delay(200);
+  xservo.attach(xservoPin);
+  yservo.attach(yservoPin);
+  pinMode(manualSwitchPin, INPUT_PULLUP);
+
+  //SD Card setup 
+  
+  // Open serial communications and wait for port to open:
+  //Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  
+}
 }
 
-/**********MAIN FUNCTION**********/
-void loop()
-{
-  readSensor();
+/**********BUOYANCY MAIN FUNCTION**********/
+void loop() {
+ // readSensor();
   if (digitalRead(ls1Pin) == 0 && digitalRead(ls2Pin) == 0) {
     
   }
@@ -103,38 +151,39 @@ void motorDriver(int motorMode){
         digitalWrite(dirPin, motorMode); // assign the mode of the motor
         digitalWrite(slpPin, motorEnable); // assign the slp pin of the driver
         analogWrite(pwmPin, motorPWM); // assign the pwm value of the motor
-        Serial.print("MODE:");
-        Serial.print(motorMode); // motor mode
-        Serial.print("\t");
-        Serial.print("PWM:");
-        Serial.print(motorPWM); // motor PWM
-        Serial.print("\t");
+        //Serial.print("MODE:");
+        //Serial.print(motorMode); // motor mode
+        //Serial.print("\t");
+        //Serial.print("PWM:");
+        //Serial.print(motorPWM); // motor PWM
+        //Serial.print("\t");
       }
       
       else {
         digitalWrite(slpPin, LOW); // turn off the motor driver
-        Serial.println("Error: motor pwm value not assingned in a correct range");
+        //Serial.println("Error: motor pwm value not assingned in a correct range");
       }
     }
     else if(motorMode == BR){
       analogWrite(pwmPin, LOW); // assign LOW to pwmPin to break
-      Serial.print("MODE:2\t"); // motor mode (BR = 2)
-      Serial.print("PWM:0\t"); // motor mode (LOW = 0)
+      //Serial.print("MODE:2\t"); // motor mode (BR = 2)
+      //Serial.print("PWM:0\t"); // motor mode (LOW = 0)
     }
     
     else {
       digitalWrite(slpPin, LOW); // turn off the motor driver
-      Serial.println("Error: motor mode not assigned correctly.");
+      //Serial.println("Error: motor mode not assigned correctly.");
     }
   }
 
   else {
     digitalWrite(slpPin, LOW); // turn off the motor driver
-    Serial.println("Error: motor driver not enabled correctly");
+    //Serial.println("Error: motor driver not enabled correctly");
   }
 }
 
 /**********READING SENSOR INPUTS**************/
+/*
 void readSensor() {
   // Read the input values
   Serial.print("FLT:");
@@ -160,6 +209,7 @@ void readSensor() {
   Serial.print("[A]");
   Serial.print("\n");
 }
+/
 
 /****CURRENT SENSOR CONVERSION*****/
 double currentSensor(int cs){
@@ -168,44 +218,24 @@ double currentSensor(int cs){
   return i;
 }
 
+
 //
-//Elec Steering joystick servo
+/*****Elec Steering joystick servo MAIN FUNCTION*****/
 //
 
-#include <Servo.h>
 
-const int joyXPin = A0;
-const int joyYPin = A1;
-const int xservoPin = 9;
-const int yservoPin = 10;
-const int manualSwitchPin = 11; 
 
-int joystick_y_in;
-int joystick_x_in;
-int manualSwitch;
-
-Servo xservo;
-Servo yservo;
-
-void setup() {
-  Serial.begin(9600);
-  delay(200);
-  xservo.attach(xservoPin);
-  yservo.attach(yservoPin);
-  pinMode(manualSwitchPin, INPUT_PULLUP);
-}
-
-void loop() {
+void loop2() {
   manualSwitch = digitalRead(manualSwitchPin);
   if (manualSwitch == 1){
     //Serial.println("Manual steering is on!");
     readInput();
     xservo.write(joystick_x_in);
     yservo.write(joystick_y_in);
-    Serial.print("X:");
-    Serial.print(joystick_x_in);
-    Serial.print(" Y:");
-    Serial.println(joystick_y_in);
+    //Serial.print("X:");
+    //Serial.print(joystick_x_in);
+    //Serial.print(" Y:");
+    //Serial.println(joystick_y_in);
   }
   else Serial.println("Manual steering is off.");
   delay(30);
@@ -232,165 +262,50 @@ int betterMap(int x, int in_min, int in_max, int out_min, int out_max) {
 }
 
 
-//SD card writing autoBollard Code
+/*****SD card writing***/
 
-// Auto-Bollard 2018
-// GPL v3
-// SUBC: the UBC Submarine Design Team
+/*
+  SD card read/write
 
-//#include "HX711.h"
-#include <SD.h>
-#include <SPI.h>
+  This example shows how to read and write data to and from an SD card file
+  The circuit:
+   SD card attached to SPI bus as follows:
+ ** MOSI - pin 11
+ ** MISO - pin 12
+ ** CLK - pin 13
+ ** CS - pin 4 (for MKRZero SD: SDCARD_SS_PIN)
 
-// Load cell calibration
-#define calibration_factor -10860.0 //calibrated with 750g test load
+  created   Nov 2010
+  by David A. Mellis
+  modified 9 Apr 2012
+  by Tom Igoe
 
-// Change when plugged in to Arduino
-#define tachoPin 2
-#define BUTTONPIN 3
-#define INDICATORPIN 9
-#define DOUT  5
-#define CLK  6
+  This example code is in the public domain.
 
-//#define BEFORECALC 4
-#define MEASUREDELAY 500
-#define CHANGEPERREV 2
-#define DELAYTIME 5
-#define DEBOUNCETIME 250
+*/
 
-// Load cell object
-HX711 scale;
-
-volatile int changes=0;
-unsigned long startTime;
-unsigned long endTime;
-unsigned long recordTime;
-int duration;
-int oldFall=0;
-boolean running=false;
-//boolean oldRunning=false;
-boolean initialized=false;
-
-/*// Debounce stuff
-int buttonState;             // the current reading from the input pin
-int lastButtonState = LOW;   // the previous reading from the input pin
-// the following variables are unsigned longs because the time, measured in
-// milliseconds, will quickly become a bigger number than can be stored in an int.
-unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
-unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers*/
-
-File outputFile;
-
-void buttonPress(void);
-void tachoChange(void);
-
-void setup() {
-  Serial.begin(9600);
-  while (!Serial){
-    // Wait for Serial to connect
-  }
-  Serial.print("Initializing SD card...");
+  //Serial.print("Initializing SD card...");
 
   if (!SD.begin(4)) {
-    Serial.println("initialization failed!");
+    //Serial.println("initialization failed!");
     while (1);
   }
-  Serial.println("initialization done.");
+  //Serial.println("initialization done.");
 
-  // Button stuff
-  pinMode(BUTTONPIN, INPUT);
-  attachInterrupt(digitalPinToInterrupt(BUTTONPIN), buttonPress, FALLING);
-  attachInterrupt(digitalPinToInterrupt(tachoPin), tachoChange, CHANGE);
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  PoolTest1 = SD.open("test.txt", FILE_WRITE);
 
-  pinMode(INDICATORPIN, OUTPUT);
-  digitalWrite(INDICATORPIN, LOW);
-
-  // Load cell initialization
-  scale.begin(DOUT, CLK);
-  scale.set_scale(calibration_factor); 
-  scale.tare();  //Assuming there is no weight on the scale at start up, reset the scale to 0
-  
-}
-
-void loop() {
-  if(running==true&&initialized==true&&millis()-startTime>MEASUREDELAY){
-    endTime=millis();
-    double RPM = (60000.0*changes/CHANGEPERREV)/(endTime-startTime);
-  
-    Serial.print("Time: ");
-    duration=(endTime-recordTime);
-    outputFile.print(duration);
-    Serial.print(duration);
-  
-    outputFile.print(",");
-  
-    Serial.print("    RPM: ");
-    Serial.print(RPM);
-    
-    /*Serial.print("   changes: ");
-    Serial.println(changes);*/
-    
-    outputFile.print(RPM);
-
-    Serial.print(scale.get_units(), 2); //"scale.get_units(), n" returns a float with n sigfigs
-    Serial.println(" N"); //Units can change but you'll need to refactor the calibration_factor
-
-    outputFile.print(",");
-    outputFile.println(scale.get_units());
-
-    changes = 0;
-    startTime=millis();
+  // if the file opened okay, write to it:
+  if (PoolTest1) {
+    //Serial.print("Writing to test.txt...");
+    PoolTest1.println("testing 1, 2, 3.");
+    // close the file:
+    PoolTest1.close();
+    //Serial.println("done.");
   } else {
-    delay(DELAYTIME);
+    // if the file didn't open, print an error:
+    //Serial.println("error opening test.txt");
   }
-}
-  
 
-void buttonPress(void){
-  Serial.println("Button falling!");
-  if (millis()-oldFall>DEBOUNCETIME){
-    oldFall=millis();
-    if (running==true&&initialized==true){
-      // Stuff to close file, etc.
-      outputFile.close();
-      Serial.println("Closing file...");
-      initialized=false;
-      running=false;
-      digitalWrite(INDICATORPIN, LOW);
-      delay(100);
-    } else if (running==false&&initialized==false){
-      // Stuff to open file, etc.
-      // create a new file
-      char filename[] = "DATA00.CSV";
-      for (uint8_t i = 0; i < 100; i++) {
-        filename[4] = i/10 + '0';
-        filename[5] = i%10 + '0';
-        if (! SD.exists(filename)) {
-          // only open a new file if it doesn't exist
-          outputFile = SD.open(filename, FILE_WRITE);
-          break;  // leave the loop!
-        }
-      }
-      outputFile.println("Time(ms),RPM,Force(N)");
-      //outputFile.println("RPM");
-      Serial.print(filename);
-      Serial.println(" successfully initialized!");
-      digitalWrite(INDICATORPIN, HIGH);
-      initialized=true;
-      running=true;
-  
-      changes=0;
-      startTime=millis();
-      recordTime=startTime;
-    } else {
-      Serial.println("Something went wrong. Trying to close file.");
-      outputFile.close();
-    }
-  } else {
-    Serial.println("Not enough time has passed since the last event. Debouncing.");
-  }
-}
-
-void tachoChange(void) {
-  changes++;
 }
