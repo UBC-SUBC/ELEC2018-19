b7ffaf40683199f280aa62fa571002d1b15f36bb
Yuki Sekimori <yuki.sekimori.8@gmail.com>
2019-06-23 08:24:14 -0700

    _buoyancy_compensator.ino new code
    I updated this file. pleas put the while statement part into the beginning of the void loop().

--- Buoyancy_Compensator/_buoyancy_compensator/_buoyancy_compensator.ino
+++ Buoyancy_Compensator/_buoyancy_compensator/_buoyancy_compensator.ino
@@ -1,15 +1,20 @@
 /*
 *  buoyancy compensator code 
-*  Last undated May 31, 2019 by Yuki Sekimori
+*  Last undated June 23, 2019 by Yuki Sekimori
 * 
 * 
 *  Things to add next:
-*  1. Sensor 
-*  2. IMU
 *  3. Control algorithm
-*  4. Serial read so that it can change the setup from the serial command.
+*  4. No more encoder :(
+*  5. KF if I can do it haha
+*  6. limit switch 
 */
 
+/*Libraries*/
+#include <Wire.h>
+#include <I2Cdev.h>
+#include <MPU6050.h> //IMU library
+#include <MS5837.h> //pressure senror library
 
 /************MOTOR SETUP***********/
 // motor on(HIGH)/off(LOW)
@@ -25,10 +30,12 @@
 * BR=2  L   X   L    L    HardBrake*
 ***********************************/
 //#define motorMode BW
+int mode = FW;
 
-// motor PWM setup
+// motor PWM 
 // Ranging from (0 - 255)
-#define motorPWM 50
+#define pwm 50
+//int pwm;
 
 
 /******Arudino pin definitions******/
@@ -37,69 +44,130 @@
 #define pwmPin 3 // pulse width modulation output pin
 #define slpPin 4 // sleep mode output pin. default LOW. 
                   // Must be set to HIGH to enable the driver
-// CAN BE REMOVED FOR SIMPLICITY
-#define fltPin 10 // fault indicator input pin. 
+// REMOVED FOR SIMPLICITY
+//#define fltPin 10 // fault indicator input pin. 
                   // This is driven LOW when fault has occurred.
-#define csPin A1 // current sense output (analog input pin)
+//#define csPin A1 // current sense output (analog input pin)
 
 // Pins connected to the motor hall sensors (input pin)
-#define hs1Pin 7
-#define hs2Pin 8
+//#define hs1Pin 7
+//#define hs2Pin 8
 // Pins connected to the limit switches (input pin)
 #define ls1Pin 12
 #define ls2Pin 13
-// Pins connected to the pressure sensor (analog input pin)
-#define psPin A2
 
-int mode;
+// IMU variables and objects
+MPU6050 IMU;
+bool IMUWorking = false;
+int16_t ax, ay, az, azPrev, oldAX=90;
+int16_t gx, gy, gz;
+
+// Buoyancy sensor (bar_02) setup
+MS5837 bar_02;
+double setDepth;
+double depth;
+bool endBuoyancy;
+
+// Limit switch setup
+int lsCount;
 
 void setup()
 {
   //setup serial communication
   Serial.begin(9600);
+  Serial.println("Starting");
+
+  Wire.begin();
+  // Initialize IMU
+  Wire.begin();
+  Serial.println( "Initializing the Sensors" );
+  IMU.initialize ( );
+  IMUWorking = IMU.testConnection();
+  if (IMUWorking == true){
+    Serial.println ("IMU Successfully Connected");
+    delay (1000);
+    Serial.println ( "Taking Values from the IMU sensor" );
+    delay (1000);
+  } else {
+    Serial.println("Connection failed");
+  }
+  
+  // initialize bar02(MS5837) pressure sensor
+  bar_02.setModel(MS5837::MS5837_02BA);
+  if (!bar_02.init()){
+    Serial.println("Pressure sensor initialization failed!");
+    delay(1000);
+  }
+  bar_02.setFluidDensity(997); // kg/m^3 (997 freshwater, 1029 for seawater)
+  bar_02.read();//read pressure sensor
+  setDepth = bar_02.depth(); //get the set depth value
+  endBuoyancy = LOW;
+  
+  
   // setup digital pin modes
   pinMode(dirPin, OUTPUT);
   pinMode(slpPin, OUTPUT);
-  pinMode(fltPin, INPUT);
-  pinMode(hs1Pin, INPUT);
-  pinMode(hs2Pin, INPUT);
+  //Motor encoder is not working anymore :(
+  //pinMode(hs1Pin, INPUT);
+  //pinMode(hs2Pin, INPUT);
   pinMode(ls1Pin, INPUT);
   pinMode(ls2Pin, INPUT);
-  pinMode(csPin, INPUT);
-  pinMode(psPin, INPUT);
+  
 }
 
 /**********MAIN FUNCTION**********/
 void loop()
 {
-  readSensors();
-  
-  int lsCount;
-  if (digitalRead(ls1Pin) == 0 && digitalRead(ls2Pin) == 0) {
-    lsCount = 0; //reset the ls Count
-  }
-  else if (digitalRead(ls1Pin) == 1 && digitalRead(ls2Pin) == 1) {
-    mode = BR; //stop the motor
-  }
-  else {
-    if (lsCount == 0 ){
-      mode = abs(mode - 1); //change the motor direction
+  while (endBuoyancy == LOW){
+    //readSensors();//input singals. For troubleshooting purpose.\
+
+    //buoyancy control algorithm
+    bar_02.read();//read pressure sensor
+    depth = bar_02.depth(); //get depth value
+    IMU.getMotion6 (&ax, &ay, &az, &gx, &gy, &gz); //get IMU values
+    if (abs(az) < 1000){ //stop the motor if the z accelearation is within the threshold
+      mode = BR; //stop the motor
+      endBuoyancy = HIGH;
     }
-    else if (lsCount > 0 && lsCount < 10) { //change the number based on experiment
-      
+    else if (abs(setDepth - bar_02.depth()) < 0.1){//move the piston until the sub is close to the set depth
+      if (abs(az - azPrev) > 0 ){
+        mode = abs(mode - 1); //change the motor direction
+      }
+      //renew the setDepth
+      setDepth = bar_02.depth();
+    }
+    
+    //check the limit sensor
+    if (digitalRead(ls1Pin) == LOW && digitalRead(ls2Pin) == LOW) {
+      lsCount = 0; //reset the ls Count
+    }
+    else if (digitalRead(ls1Pin) == HIGH && digitalRead(ls2Pin) == HIGH) {
+      mode = BR; //stop the motor
     }
     else {
-    mode = BR; //stop the motor
+      if (lsCount == 0 ){
+        mode = abs(mode - 1); //change the motor direction
+      }
+      else if (lsCount > 0 && lsCount < 10) { //change the number based on experiment
+        
+      }
+      else {
+      mode = BR; //stop the motor
+      endBuoyancy = HIGH; 
+      break;
+      }
     }
-  }
-  lsCount++;
-  motorDriver(mode);
+    lsCount++;
+      
+    motorDriver(mode,pwm);
+    }
+
 }
 
 /********FUNCTIONS********/
 
 /***************MOTOR DRIVER****************/
-void motorDriver(int motorMode){
+void motorDriver(int motorMode, int motorPwm){
   // Check if the motor enable is assingned properly
   if (motorEnable == HIGH || motorEnable == LOW){
     
@@ -107,15 +175,15 @@ void motorDriver(int motorMode){
     if (motorMode == FW || motorMode == BW){
 
       // Check if the pwm value is within a correct range
-      if (motorPWM >= 0 || motorPWM <= 255){ 
+      if (motorPwm >= 0 || motorPwm <= 255){ 
         digitalWrite(dirPin, motorMode); // assign the mode of the motor
         digitalWrite(slpPin, motorEnable); // assign the slp pin of the driver
-        analogWrite(pwmPin, motorPWM); // assign the pwm value of the motor
+        analogWrite(pwmPin, motorPwm); // assign the pwm value of the motor
         Serial.print("MODE:");
         Serial.print(motorMode); // motor mode
         Serial.print("\t");
         Serial.print("PWM:");
-        Serial.print(motorPWM); // motor PWM
+        Serial.print(motorPwm); // motor PWM
         Serial.print("\t");
       }
       
@@ -145,33 +213,36 @@ void motorDriver(int motorMode){
 /**********READING SENSOR INPUTS**************/
 void readSensors() {
   // Read the input values
-  Serial.print("FLT:");
-  Serial.print(digitalRead(fltPin)); // FLT pin readings
+  Serial.print("READ SENSOR:\t");
+  //Serial.print("FLT:");
+  //Serial.print(digitalRead(fltPin)); // FLT pin readings
   Serial.print("\t");
-  Serial.print("HS1:");
-  Serial.print(digitalRead(hs1Pin)); // hall sensor 1 readings
-  Serial.print("\t");
-  Serial.print("HS2:");
-  Serial.print(digitalRead(hs2Pin)); // hall sensor 2 readings
-  Serial.print("\t");
+  //Serial.print("HS1:");
+  //Serial.print(digitalRead(hs1Pin)); // hall sensor 1 readings
+  //Serial.print("\t");
+  //Serial.print("HS2:");
+  //Serial.print(digitalRead(hs2Pin)); // hall sensor 2 readings
+  //Serial.print("\t");
   Serial.print("LS1:");
   Serial.print(digitalRead(ls1Pin)); // limit switch 1 readings
   Serial.print("\t");
   Serial.print("LS2:");
   Serial.print(digitalRead(ls2Pin)); // limit switch 2 readings
   Serial.print("\t");
-  Serial.print("PS:");
-  Serial.print(analogRead(psPin)); // pressure sensor readings
+  bar_02.read();
+  Serial.print("Pressure: "); 
+  Serial.print(bar_02.pressure()); 
+  Serial.print(" mbar");
   Serial.print("\t");
-  Serial.print("CS:");
-  Serial.print(currentSensor(analogRead(csPin))); // current sensor readings
-  Serial.print("[A]");
-  Serial.print("\n");
-}
-
-/****CURRENT SENSOR CONVERSION*****/
-double currentSensor(int cs){
-  double i;
-  i = (cs * 5 / 1024 -0.05)/ 0.02; //A
-  return i;
+  Serial.print("Temperature: "); 
+  Serial.print(bar_02.temperature()); 
+  Serial.print(" deg C");
+  Serial.print("\t");
+  Serial.print("Depth: "); 
+  Serial.print(bar_02.depth()); 
+  Serial.print(" m");
+  Serial.print("\t");
+  Serial.print("Altitude: "); 
+  Serial.print(bar_02.altitude()); 
+  Serial.println(" m above mean sea level");
 }
